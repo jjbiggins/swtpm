5ee2155a6548062f8aec733c7640c4f20b6be084
Sourcery AI <>
2022-06-20 05:57:03 +0000

    'Refactored by Sourcery'

--- tests/test_setdatafd.py
+++ tests/test_setdatafd.py
@@ -15,9 +15,6 @@ def toString(arr):
 
 def test_ReadPCR10(fd):
     send_data = bytearray(b"\x00\xC1\x00\x00\x00\x0C\x00\x00\x00\x99\x00\x01")
-    exp_data = bytearray([0x00, 0xC4, 0x00, 0x00, 0x00, 0x0A,
-                          0x00, 0x00, 0x00, 0x26])
-
     try:
         print("Sending data over ....")
         n = fd.send(send_data)
@@ -30,27 +27,28 @@ def test_ReadPCR10(fd):
     buf = fd.recv(1024)
     fd.close()
     if buf:
+        exp_data = bytearray([0x00, 0xC4, 0x00, 0x00, 0x00, 0x0A,
+                              0x00, 0x00, 0x00, 0x26])
+
         if bytearray(buf) == exp_data:
             return True
-        else:
-            print("Unexpected reply:\n  actual: %s\n  expected: %s"
-                  % (toString(buf), toString(exp_data)))
-            return False
+        print("Unexpected reply:\n  actual: %s\n  expected: %s"
+              % (toString(buf), toString(exp_data)))
     else:
         print("Null reply from swtpm")
-        return False
+
+    return False
 
 
 def test_SetDatafd():
     fd, _fd = socket.socketpair(socket.AF_UNIX, socket.SOCK_DGRAM)
     sock_path = os.getenv('SOCK_PATH')
     cmd_set_data_fd = bytearray([0x00, 0x00, 0x00, 0x10])
-    expected_res = bytearray([0x00, 0x00, 0x00, 0x00])
     try:
         fds = array("i")
         fds.append(_fd.fileno())
         ctrlfd = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        print("Connecting to server at : %s" % sock_path)
+        print(f"Connecting to server at : {sock_path}")
         ctrlfd.connect(sock_path)
         print("Sending data fd over ctrl fd...")
         if sys.version_info[0] < 3:
@@ -62,30 +60,27 @@ def test_SetDatafd():
             ctrlfd.sendmsg([cmd_set_data_fd],
                            [(socket.SOL_SOCKET, socket.SCM_RIGHTS, fds)])
     except socket.error as e:
-        print("SocketError: " + str(e))
+        print(f"SocketError: {str(e)}")
         ctrlfd.close()
 
     buf = ctrlfd.recv(4)
-    print("Received bytes.. : %s" % buf)
+    print(f"Received bytes.. : {buf}")
     if buf:
         caps = bytearray(buf)
+        expected_res = bytearray([0x00, 0x00, 0x00, 0x00])
         if caps == expected_res:
             return test_ReadPCR10(fd)
-        else:
-            print("Unexpected reply for CMD_SET_DATA_FD: \n"
-                  "  actual: %s\n  expected: %s"
-                  % (toString(caps), toString(expected_res)))
-            return False
+        print("Unexpected reply for CMD_SET_DATA_FD: \n"
+              "  actual: %s\n  expected: %s"
+              % (toString(caps), toString(expected_res)))
     else:
         print("Null reply from swtpm")
-        return False
+
+    return False
 
 if __name__ == "__main__":
     try:
-        if not test_SetDatafd():
-            res = 1
-        else:
-            res = 0
+        res = 0 if test_SetDatafd() else 1
     except:
         print("__Exception: ", sys.exc_info())
         res = -1
